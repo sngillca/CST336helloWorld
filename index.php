49e3ec423c1820785f85c0e1f2d4a136ce1273a6
sngillca <sngill.ca@gmail.com>
2018-09-04 22:23:21 -0700

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,17 @@
-Hello World
+<!DOCTYPE <!DOCTYPE html>
+<html>
+<!--
+
+First Website
+Author: Natalia Gill
+CST 336
+09/05/18
+
+-->
+
+<head>
+    <meta charset="utf-8" />
+    <title>Natalia Gill: Personal Website</title>
+</head>
+    
+</html>
