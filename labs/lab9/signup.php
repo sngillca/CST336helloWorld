6cde2e1ed1e8fd7792ed8a2fbeb2f3f44d044b1a
Natalia <sigill@csumb.edu>
2018-12-06 19:05:47 +0000

    due

--- labs/lab9/signup.php
+++ labs/lab9/signup.php
@@ -5,13 +5,22 @@
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
         <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
         <script type="text/javascript" src="/labs/lab9/main/js/index.js" type= "module"></script>
+        <style type="text/css">
+            a{
+                color: green;
+            }
+            body{
+                color: grey;
+            }
+        </style>
     </head>
     
     <body>
-        <main class="container row align center">
-            <h1>Sign Up</h1>
+        <main class="container-fluid">
+            <h1><kbd>Sign Up</kbd></h1>
              <form class="col l8 offset-l2">
                 <div class="row">
+                    
                     <div class="input-field col s6">
                       <input placeholder="Username" id="username" type="text">
                     </div>
@@ -40,7 +49,7 @@
                 <br>
                 <p id="form-status" class="error"></p>
                 <br>
-                <a id="confirm" class="waves-effect waves-light btn">Press to Continue</a>
+                <a id="confirm">Press to Continue</a>
             </form>
             <div id="city-cont" class="col s12">
                 <h3 id="city"></h3>
