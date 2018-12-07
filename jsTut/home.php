c4d6583354cc28cd8b772197922891604d9bff2b
Natalia <sigill@csumb.edu>
2018-12-07 04:51:13 +0000

    js

---
+++ jsTut/home.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+      <title>The Marauder's Map</title>
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+      <style type="text/css">
+          body{
+                    background-color:#332239;
+                    
+                    
+                }
+         
+      </style>
+    </head>
+    <body background="/jsTut/hall.jpg">
+    
+    <nav class="navbar navbar-inverse">
+      <div class="container-fluid">
+        <div class="navbar-header">
+          <a class="navbar-brand" href="/jsTut/home.php">The Marauder's Map <small>For Muggles & Pure-Bloods</small></a>
+        </div>
+        <ul class="nav navbar-nav">
+          <li ><a href="/jsTut/testDB.php">Use Map</a></li>
+          <li><a href="/jsTut/pat.php">Find Your Patronus</a></li>
+          <li><a href="/jsTut/hos.php">Find Your House</a></li>
+          <li><a href="/jsTut/login.php">Login</a></li>
+        </ul>
+      </div>
+    </nav>
+    
+    
+    <script type="text/javascript" src="/jsTut/main.js"></script>
+    </body>
+</html>
--- jsTut/main.js
+++ jsTut/main.js
@@ -17,6 +17,31 @@ function search(){
     var nameIn = document.getElementById("name").value;
     var name = ""+nameIn;
     name = name.toLowerCase();
+    
+    var lastIn = document.getElementById("lastName").value;
+    var lastName = ""+lastIn;
+    lastName.toLowerCase();
+    
+    var patIn = document.getElementById("patronus").value;
+    var patronus = ""+patIn;
+    patronus.toLowerCase();
+    
+    var houseIn = document.getElementById("house").value;
+    var house;
+    if(house == "0"){
+        house = "none";
+    } else if(houseIn == "1" ) {
+        house = "gryffindor";
+    } else if(houseIn == "2"){
+        house = "hufflepuff";
+    } else if (houseIn == "3") {
+        house = "ravenclaw";
+    } else if (houseIn == "4"){
+        house = "slytherin";
+        
+    } 
+    
+
     //call ajax
     var ajax = new XMLHttpRequest();
     var method = "GET";
@@ -37,20 +62,35 @@ function search(){
             var html = "";
             var count = 0;
             for(var a = 0; a < data.length; a++){
-                var firstName = data[a].firstName;
-                if(firstName == name){
+                var firstNamedb = data[a].firstName;
+                var lastNamedb = data[a].lastName;
+                var housedb = data[a].house;
+                var patronusdb = data[a].patronus;
+                if(firstNamedb == name || lastNamedb == lastName ){
                     html += "<tr>";
-                        html += "<td>" +firstName+ "</td>";
+                        html += "<h2><em><td><kbd>Witch/Wizard: " +firstNamedb+" "+lastNamedb+" - Patronus: "+patronusdb+" - House:  "+housedb+"</kbd></em></h2></td><br>";
+                        
                     html += "</tr>";
                     count +=1;
-                } if(count == 0 && a == data.length-1) {
-                    html += "No Witch or Wizard found.";
-                    
+                } if(house == "0" && count > 0){
+                     html += "<h2><em><td><kbd>Witch/Wizard: " +firstNamedb+" "+lastNamedb+" - Patronus: "+patronusdb+" - House:  "+housedb+"</kbd></em></h2></td><br>";
+                    count = 0;
+                }
+                else if (housedb == house || patronusdb == patronus) {
+                    count+=1;
+                    html += "<h2><em><td><kbd>Witch/Wizard: " +firstNamedb+" "+lastNamedb+" - Patronus: "+patronusdb+" - House:  "+housedb+"</kbd></em></h2></td><br>";
                 } 
                 
+                if(count == 0 && a == data.length-1) {
+                    html += "<h2><em><td><kbd>No Witch(es) or Wizard(s) found.</kbd></em></h2>";
+                    
+                } 
             }//for
-            document.getElementById("data").innerHTML = html;
+            document.getElementById("myDIV").innerHTML = html;
             
         }//if
-    }
+    }//onreadychange
+    
+    
 }//search()
+
